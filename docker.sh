77bd370d3cd752090f4eacb9ebed16accc7b26f2
Felipe Pina <felipe.pina@pobox.com>
2020-04-17 11:42:42 -0300

    Add Docker instalation script to Debian/Ubuntu/Pop!_OS

---
+++ docker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+apt-get update
+
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+
+# apt-key fingerprint 0EBFCD88
+
+add-apt-repository -y \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+
+apt-get update
+
+apt-get install -y docker-ce docker-ce-cli containerd.io
