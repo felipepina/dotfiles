3287efde8dfda863b03265448c5273fec4874461
Felipe Pina <felipe.pina@pobox.com>
2020-05-20 05:57:08 -0300

    Update apps installation scripts

--- flatpak.sh
+++ flatpak.sh
@@ -1,23 +1,36 @@
 #!/usr/bin/env bash
 
-# Install Snap Daemon
+# Install Flatpak
 sudo apt install flatpak
 
+# Add Flathub repository
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-# A simple Git commit message editor for Gnome (https://github.com/indie-mirror/gnomit)
-flatpak install flathub ind.ie.Gnomit
-# git config --global core.editor "flatpak run ind.ie.Gnomit"
-
+# Install apps
+flatpak install flathub com.basemark.BasemarkGPU
 flatpak install flathub com.calibre_ebook.calibre
+flatpak install flathub com.github.alainm23.planner
+flatpak install flathub com.github.bilelmoussaoui.Authenticator
 flatpak install flathub com.github.johnfactotum.Foliate
 flatpak install flathub com.github.mdh34.quickdocs
-flatpak install flathub ind.ie.Gnomit
+flatpak install flathub com.github.tchx84.Flatseal
+flatpak install flathub com.leinardi.gst
+flatpak install flathub com.leinardi.gwe
+flatpak install flathub com.slack.Slack
+flatpak install flathub com.spotify.Client
 flatpak install flathub io.github.celluloid_player.Celluloid
 flatpak install flathub org.darktable.Darktable
+flatpak install flathub org.freedesktop.Piper
 flatpak install flathub org.gimp.GIMP
 flatpak install flathub org.gnome.Books
+flatpak install flathub org.gnome.Lollypop
+flatpak install flathub org.gnome.Rhythmbox3
 flatpak install flathub org.gnome.Shotwell
+flatpak install flathub org.gnucash.GnuCash
 flatpak install flathub org.inkscape.Inkscape
 flatpak install flathub org.kde.krita
+flatpak install flathub org.mozilla.Thunderbird
 flatpak install flathub org.remmina.Remmina
-
+flatpak install flathub org.small_tech.Gnomi # A simple Git commit message editor for Gnome (https://github.com/small-tech/gnomit)
+# git config --global core.editor "flatpak run org.small_tech.Gnomit"
+flatpak install flathub org.telegram.desktop
--- snap.sh
+++ snap.sh
@@ -4,18 +4,11 @@
 sudo apt install snapd
 
 # Install snaps
-sudo snap install intellij-idea-community --classic
+sudo snap install chromium
 sudo snap install code --classic
-sudo snap install slack --classic
-sudo snap install shfmt            # A shell parser, formatter and interpreter (POSIX/Bash/mksh) (https://github.com/mvdan/sh)
-sudo snap install remmina          # Remote Desktop Client (https://remmina.org)
-sudo snap install telegram-desktop # Official desktop client for the Telegram messenger (https://desktop.telegram.org)
-sudo snap install foliate          # A simple and modern eBook viewer (https://johnfactotum.github.io/foliate)
-sudo snap install plexmediaserver  # Plex magically organizes your media libraries and streams them to any device (https://www.plex.tv)
-sudo snap install spotify          # Music for everyone (https://www.spotify.com)
+sudo snap install intellij-idea-community --classic
+sudo snap install kubectl --classic
 sudo snap install ncspot           # Cross-platform ncurses Spotify client written in Rust (https://github.com/hrkfdn/ncspot)
-sudo snap install musixmatch       # Musixmatch gives you the lyrics of your favorite songs (https://www.musixmatch.com)
+sudo snap install plexmediaserver  # Plex magically organizes your media libraries and streams them to any device (https://www.plex.tv)
+sudo snap install shfmt            # A shell parser, formatter and interpreter (POSIX/Bash/mksh) (https://github.com/mvdan/sh)
 sudo snap install simplenote       # The simplest way to keep notes (https://github.com/snapcrafters/simplenote)(https://simplenote.com/)
-sudo snap install inkscape         # Vector Graphics Editor (https://inkscape.org)
-sudo snap install gimp             # The Free & Open Source Image Editor (https://www.gimp.org)
-sudo snap install krita            # Digital Painting, Creative Freedom (https://krita.org)
