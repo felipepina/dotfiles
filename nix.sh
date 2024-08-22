fe3e5d0c8fb55804e6c423cf41f6fbed0b34519d
Felipe Pina <fpina@tecgraf.puc-rio.br>
2024-08-22 18:18:47 -0300

    add Home MAnager installation to nix.sh

--- nix.sh
+++ nix.sh
@@ -9,3 +9,6 @@ nix-channel --update nixpkgs
 # Install Home Manager
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
+. ${HOME}/.nix-profile/etc/profile.d/nix.sh
+nix-shell '<home-manager>' -A install
+
