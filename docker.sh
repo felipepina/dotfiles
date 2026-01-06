7054d47431f3786400123fa031498e1f06bdda51
Felipe Pina <felipe.pina@pobox.com>
2026-01-06 15:43:45 -0300

    update the Docker install script

--- docker.sh
+++ docker.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-apt-get update
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+sudo apt update
 
-# apt-key fingerprint 0EBFCD88
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-add-apt-repository -y \
-    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-    $(lsb_release -cs) \
-    stable"
+sudo groupadd docker
 
-apt-get update
-
-apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker $USER
