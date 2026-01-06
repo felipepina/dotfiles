af9e65b65cfeaecc47a89c11845aeddf4f09fd1d
Felipe Pina <felipe.pina@pobox.com>
2026-01-06 15:44:12 -0300

    fix the Nix install script

--- nix.sh
+++ nix.sh
@@ -3,12 +3,13 @@
 # Install Nix
 sh <(curl -L https://nixos.org/nix/install) --no-daemon
 
+. ${HOME}/.nix-profile/etc/profile.d/nix.sh
+
 # Update channel and packages
 nix-channel --update nixpkgs
 
 # Install Home Manager
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
-. ${HOME}/.nix-profile/etc/profile.d/nix.sh
 nix-shell '<home-manager>' -A install
 
