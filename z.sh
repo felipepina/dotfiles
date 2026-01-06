2536c0d9e60858272ca87189bed2ecb88509743b
Felipe Pina <felipe.pina@pobox.com>
2026-01-06 15:42:05 -0300

    patch the zfz.sh shell script

--- z.sh
+++ z.sh
@@ -6,3 +6,6 @@ curl "https://raw.githubusercontent.com/rupa/z/master/{z.sh}" \
 # The missing fuzzy tab completion feature of z jump around command (https://github.com/changyuheng/fz)
 curl "https://raw.githubusercontent.com/changyuheng/fz/master/{fz.sh}" \
 	-o "${HOME}/dotfiles/bin/z""#1"
+
+# Replace the literal '${=fzf}' with 'eval "$fzf"' in the installed zfz helper
+sed -i 's/\${=fzf}/eval "\$fzf"/g' "${HOME}/dotfiles/bin/zfz.sh"
